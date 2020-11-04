809da411c49173f27ff230ca1884e647025b8339
Usairim Isani <usairim.isani@outlook.com>
2020-11-04 19:10:12 +0500

    Exercises Completed.

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -23,6 +22,23 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            Err(String::from("No name specified."))
+        } else {
+            let args = s.split(",").collect::<Vec<&str>>();
+            if args.len() == 2 {
+                let name = if !args[0].is_empty() {
+                    args[0].to_string()
+                } else {
+                    panic!("Invalid Name")
+                };
+                let age = args[1].parse::<usize>().unwrap();
+                Ok(Person { name, age })
+            } else {
+                // Err(String::from("Invalid"))
+                panic!("Invalid input")
+            }
+        }
     }
 }
 
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,11 +7,15 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn ..... {
-
+fn calculate_apple_price(val: i32) -> i32 {
+    if val > 40 {
+        val * 1
+    } else {
+        val * 2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
